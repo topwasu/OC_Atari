d05de55092bc713986a1f3280a98079e17e95749
topwasu <wasu.piri@gmail.com>
2025-03-04 17:05:27 -0500

    Fix wall bounding boxes

--- ocatari/ram/pong.py
+++ ocatari/ram/pong.py
@@ -223,8 +223,8 @@ def _detect_objects_ram(objects, ram_state, hud=False):
                 
     if len(objects) == 5:
         # side walls
-        objects.append(Wall(1, 29, 158, 5))
-        objects.append(Wall(1, 189+4, 158, 5))
+        objects.append(Wall(5, 29, 150, 5))
+        objects.append(Wall(5, 190+5, 150, 5))
         
         # walls behind player
         objects.append(Zone(0, 29, 5, 169))

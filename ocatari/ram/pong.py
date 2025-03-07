013213944841cb1d6cefdd9e908d0107fbf36027
topwasu <wasu.piri@gmail.com>
2025-03-07 10:24:28 -0500

    Fix pong wall again

--- ocatari/ram/pong.py
+++ ocatari/ram/pong.py
@@ -224,7 +224,7 @@ def _detect_objects_ram(objects, ram_state, hud=False):
     if len(objects) == 5:
         # side walls
         objects.append(Wall(6, 29, 148, 5))
-        objects.append(Wall(6, 190+5, 148, 5))
+        objects.append(Wall(6, 190+4, 148, 5))
         
         # walls behind player
         objects.append(Zone(0, 29, 5, 169))

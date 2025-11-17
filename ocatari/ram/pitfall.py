6d5317d2c5a9c8561e7bc5af18788ad44f33eda9
topwasu <wasu.piri@gmail.com>
2025-11-17 16:20:34 -0500

    Fix max number of objects for pitfall

--- ocatari/ram/pitfall.py
+++ ocatari/ram/pitfall.py
@@ -12,8 +12,11 @@ RAM extraction for the game Pitfall.
 """
 
 MAX_NB_OBJECTS = {"Player": 1, "Wall": 1, "Logs": 5, "StairPit": 1, "Stair": 1, "Pit": 3, "Scorpion": 1, "Rope": 1, "Snake": 1,
-                  "Tarpit": 1, "Waterhole": 1, "Crocodile": 3, "GoldenBar": 1, "Fire": 1, "Platform": 4}
-MAX_NB_OBJECTS_HUD = {"LifeCount": 3, "PlayerScore": 6, "Timer": 5}
+                  "Tarpit": 1, "Waterhole": 1, "Crocodile": 3, "GoldenBar": 1, "Fire": 1, "Platform": 4, 'DisappearingTarpit': 1,
+                  'MovingLogs': 3, 'ClosedCrocodile': 3, 'OpenCrocodile': 3, 'DangerousPlatform': 1, 
+                  'Portal_to_prev_room': 1, 'Portal_to_next_room': 1}
+roomnumber_objects = {f'RoomNumber_{i}': 1 for i in range(7)}
+MAX_NB_OBJECTS_HUD = {**MAX_NB_OBJECTS, "LifeCount": 3, "PlayerScore": 6, "Timer": 5, **roomnumber_objects}
 
 
 class Player(GameObject):

48bcf40c5966f6c762f846622580009e0d2e9918
topwasu <wasu.piri@gmail.com>
2025-11-17 16:39:46 -0500

    Fix issue in Pitfall max_nb_objects

--- ocatari/ram/pitfall.py
+++ ocatari/ram/pitfall.py
@@ -442,7 +442,10 @@ def _get_max_objects(hud=False):
         mod = sys.modules[__name__]
         for k, v in max_obj_dict.items():
             for _ in range(0, v):
-                objects.append(getattr(mod, k)())
+                if '_' in k:
+                    objects.append(getattr(mod, k.split('_')[0])(k.split('_')[1]))
+                else:
+                    objects.append(getattr(mod, k)())
         return objects
 
     if hud:
